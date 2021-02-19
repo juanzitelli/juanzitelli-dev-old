46aec110b9d7fa65c147767c6f4680f849e41434
Juan Agustín Zitelli <juanzitelli7@gmail.com>
2021-02-19 02:13:18 -0300

    Feature: Removed unnecessary spaces and comments from index

--- pages/index.tsx
+++ pages/index.tsx
@@ -28,14 +28,12 @@ const Home = () => {
 
   return (
     <Layout title={`${locale === "es" ? "Inicio" : "Home"}`}>
-
       <div className="w-full h-screen bg-opacity-50 flex justify-center items-center animate__animated animate__fadeIn">
         <div className="lg:mx-4 md:mx-4 lg:mt-0 md:mt-0 mb pb-10 text-center text-white w-full sm:w-4/5">
           <div className="flex flex-col items-center justify-center">
             <Image alt="Juan Agustín Zitelli" loading="lazy" layout="intrinsic" className="rounded-full my-12" height="160" width="160" src="/img/profile.jpg" />
             <div>
               <h1 className="font-bold text-4xl md:text-7xl mb-4 sm:my-1 my-7">Juan Agustín Zitelli</h1>
-              {/* <h2 className="font-regular text-2xl md:text-3xl mx-5 mb-6 md:mt-5 h-24 sm:h-10">{activeQuote}</h2> */}
               <div className="h-15">
                 <h2 className="font-regular text-xl md:text-2xl md:mt-5 h-full h-1/3 md:h-auto ">{activeQuote}</h2>
               </div>
@@ -47,14 +45,11 @@ const Home = () => {
                 {t.home.getToKnowMeButton}
               </a>
             </Link>
-
-            {/* public/pdf/cv */}
             <a rel="noopener" target="_blank" href={`/pdf/cv/${locale!.toUpperCase()} - Juan Agustín Zitelli - Currículum Vitae.pdf`} className="bg-gray-900 rounded-md font-bold text-white text-center transition duration-300 ease-in-out hover:bg-gray-600 px-4 py-3">
               {t.home.downloadCVButton}
             </a>
           </div>
         </div>
-
       </div>
     </Layout>
   )
