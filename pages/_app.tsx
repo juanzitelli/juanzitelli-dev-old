72a2de7a7b08ef45213d623a9db0ccf10232ea42
Juan Agustín Zitelli <juanzitelli7@gmail.com>
2021-02-19 02:14:58 -0300

    Feature: Made react-query accessible for the entire app

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,17 @@
 import './../styles/index.css'
+import { QueryClient, QueryClientProvider } from "react-query";
 interface AppProps {
 	Component: React.FC;
 	pageProps: { [prop: string]: any }
 }
 
 const App = ({ Component, pageProps }: AppProps) => {
-	return <Component {...pageProps} />
+	const queryClient = new QueryClient();
+	return (
+		<QueryClientProvider client={queryClient}>
+			<Component {...pageProps} />
+		</QueryClientProvider>
+	)
 }
 
 export default App
